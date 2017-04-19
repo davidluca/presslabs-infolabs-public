ff44192d4406560950c48f67a1a5d35a9ed78592
davidluca <David>
2017-04-19 13:38:38 +0300

    am creat clasele si am facut modificari dupa tutorialul din DRF

--- domainCheck/models.py
+++ domainCheck/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class DomainCheck(models.Model):
+	domain = models.CharField(max_length=200)
+	state = models.CharField(max_length=200)
+	create_date = models.DateTimeField()
+	feature = models.ForeignKey('Feature',
+		on_delete=models.CASCADE)
+
+class Feature(models.Model):
+	name = models.CharField(max_length=200)
+	value = models.CharField(max_length=200)
+	compare_value = models.CharField(max_length=200)
--- domainCheck/views.py
+++ domainCheck/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from domainCheck.models import DomainCheck, Feature
+from rest_framework import viewsets
+from infolabs.domainCheck.serializers import DomainCheckSerializer, FeatureSerializer
 
-# Create your views here.
+
+class DomainCheckViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = DomainCheck.objects.all().order_by('-date_joined')
+    serializer_class = DomainCheckSerializer
+
+
+class FeatureViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Feature.objects.all()
+    serializer_class = FeatureSerializer
