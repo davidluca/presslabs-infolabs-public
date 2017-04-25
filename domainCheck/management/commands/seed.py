02f800d0849d3193cbe74c10126d0ddbc6e448cc
arpadgabor <arpad.gabor@ligaac.ro>
2017-04-25 20:29:22 +0300

    Changed based on comments

--- domainCheck/management/commands/seed.py
+++ domainCheck/management/commands/seed.py
@@ -1,21 +1,23 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from django_dynamic_fixture import G, new
+from django_dynamic_fixture import G
 from domainCheck.models import Report, Feature
 
 
 class Command(BaseCommand):
-	help = 'Help.'
-	def handle(self, *args, **options):
-		report1 = G(Report)
-		report2 = G(Report)
-		# Generate Features
-		feature1 = G(Feature, Report = report1)
-		feature2 = G(Feature, Report = report1)
-		feature3 = G(Feature, Report = report2)
-		feature4 = G(Feature, Report = report2)
-		# Generate Admin
-		admin = User.objects.get_or_create(username = 'admin')
-		admin.is_staff =True
-		admin.set_password("admin")
-		admin.save()
+    help = 'Initial generated data.'
+    def handle(self, *args, **options):
+        report1 = G(Report)
+        report2 = G(Report)
+        # Generate Features
+        feature1 = G(Feature, report = report1)
+        feature2 = G(Feature, report = report1)
+        feature3 = G(Feature, report = report2)
+        feature4 = G(Feature, report = report2)
+        # Generate Admin
+        admin, _ = User.objects.get_or_create(username = 'admin')
+        admin.is_staff =True
+        admin.is_superuser = True
+        admin.is_admin = True
+        admin.set_password("admin")
+        admin.save()
