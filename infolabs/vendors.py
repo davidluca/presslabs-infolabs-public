be88fa90de782d5b4721fc8ff184da7837fded70
RzvnB <bala.razvan15@gmail.com>
2017-04-25 19:02:42 +0300

    deleted debug task from vendors

--- infolabs/vendors.py
+++ infolabs/vendors.py
@@ -3,13 +3,8 @@ import os
 from celery import Celery
 
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infolabs.settings')
 
 celery_app = Celery('infolabs')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
-
-@celery_app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
