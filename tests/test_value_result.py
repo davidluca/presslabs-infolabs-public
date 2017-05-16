ed657418fbe0b26ad7f118152e5ede041fcb2f14
davidluca <David>
2017-05-16 20:33:19 +0300

    value_result = edited test_value_result = created

---
+++ tests/test_value_result.py
@@ -0,0 +1,34 @@
+import pytest
+from domainCheck.value_result import ValueResult, TableValueResult
+
+
+def test_validate_ValueResultObj():
+    valueObj = ValueResult(14)
+    assert valueObj.get_value() == {
+        'type': 'ValueResult', 'value': 14}
+
+
+def test_validate_wrong_input():
+    with pytest.raises(ValueError):
+        ValueResult({'1': 1})
+
+
+def test_validate_TableValueResultObj():
+    tableObj = TableValueResult([(1, 2, 3), (1, 2)])
+    assert tableObj.get_value() == {
+        'type': 'TableValueResult',
+        'value': [(1, 2, 3), (1, 2)]
+    }
+
+
+def test_validate_list_of_non_tuples():
+    with pytest.raises(ValueError):
+        ValueResult([1, 2, 3])
+
+
+def test_validate_dict():
+    tableObj = TableValueResult({'1': 1, '2': 2})
+    assert tableObj.get_value() == {
+        'type': 'TableValueResult',
+        'value': [('2', 2), ('1', 1)]
+    }
